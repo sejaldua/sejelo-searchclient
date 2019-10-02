6b9cb6985c4c410b6c472e047b3e4f32bd2d17c7
Sejal <sejal.dua@tufts.edu>
2019-10-03 00:43:15 +0200

    Implement Exercise 4: Sokoban Mode, now making more levels

--- searchclient_java/other.sh
+++ searchclient_java/other.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-java -jar ../server.jar -l ../levels/SAlabyrinth.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAlabyrinth_BFS.txt 2>&1
+# LABYRINTHS
+# java -jar ../server.jar -l ../levels/SAlabyrinth.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAlabyrinth_BFS.txt 2>&1
 
-java -jar ../server.jar -l ../levels/SAlabyrinth.lvl -c "java -Xmx8g searchclient.SearchClient -dfs" -g -s 150 -t 180 > ../other_levels/SAlabyrinth_DFS.txt 2>&1
+# java -jar ../server.jar -l ../levels/SAlabyrinth.lvl -c "java -Xmx8g searchclient.SearchClient -dfs" -g -s 150 -t 180 > ../other_levels/SAlabyrinth_DFS.txt 2>&1
 
-java -jar ../server.jar -l ../levels/SAlabyrinthOfStBertin.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAlabyrinthOfStBertin_BFS.txt 2>&1
+# java -jar ../server.jar -l ../levels/SAlabyrinthOfStBertin.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAlabyrinthOfStBertin_BFS.txt 2>&1
 
-java -jar ../server.jar -l ../levels/SAlabyrinthOfStBertin.lvl -c "java -Xmx8g searchclient.SearchClient -dfs" -g -s 150 -t 180 > ../other_levels/SAlabyrinthOfStBertin_DFS.txt 2>&1
+# java -jar ../server.jar -l ../levels/SAlabyrinthOfStBertin.lvl -c "java -Xmx8g searchclient.SearchClient -dfs" -g -s 150 -t 180 > ../other_levels/SAlabyrinthOfStBertin_DFS.txt 2>&1
 
-java -jar ../server.jar -l ../levels/SAmicromousecontest2011.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAmicromousecontest2011_BFS.txt 2>&1
+# java -jar ../server.jar -l ../levels/SAmicromousecontest2011.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAmicromousecontest2011_BFS.txt 2>&1
 
-java -jar ../server.jar -l ../levels/SAmicromousecontest2011.lvl -c "java -Xmx8g searchclient.SearchClient -dfs" -g -s 150 -t 180 > ../other_levels/SAmicromousecontest2011_DFS.txt 2>&1
+# java -jar ../server.jar -l ../levels/SAmicromousecontest2011.lvl -c "java -Xmx8g searchclient.SearchClient -dfs" -g -s 150 -t 180 > ../other_levels/SAmicromousecontest2011_DFS.txt 2>&1
+
+java -jar ../server.jar -l ../levels/SAsokoban01.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban01.txt 2>&1
 
