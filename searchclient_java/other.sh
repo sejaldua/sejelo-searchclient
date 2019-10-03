f08266bd76b048c56785f0fe0ace550491f4c608
Sejal <sejal.dua@tufts.edu>
2019-10-03 14:04:36 +0200

    Add Sokoban levels 70-74

--- searchclient_java/other.sh
+++ searchclient_java/other.sh
@@ -13,5 +13,23 @@
 
 # java -jar ../server.jar -l ../levels/SAmicromousecontest2011.lvl -c "java -Xmx8g searchclient.SearchClient -dfs" -g -s 150 -t 180 > ../other_levels/SAmicromousecontest2011_DFS.txt 2>&1
 
-java -jar ../server.jar -l ../levels/SAsokoban01.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban01.txt 2>&1
+# java -jar ../server.jar -l ../levels/SAsokoban01.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban01.txt 2>&1
+
+# java -jar ../server.jar -l ../levels/SAsokoban02.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban02.txt 2>&1
+
+# java -jar ../server.jar -l ../levels/SAsokoban03.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban03.txt 2>&1
+
+# java -jar ../server.jar -l ../levels/SAsokoban04.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban04.txt 2>&1
+
+# java -jar ../server.jar -l ../levels/SAsokoban35.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban35.txt 2>&1
+
+java -jar ../server.jar -l ../levels/SAsokoban70.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban70.txt 2>&1
+
+java -jar ../server.jar -l ../levels/SAsokoban71.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban71.txt 2>&1
+
+java -jar ../server.jar -l ../levels/SAsokoban72.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban72.txt 2>&1
+
+java -jar ../server.jar -l ../levels/SAsokoban73.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban73.txt 2>&1
+
+java -jar ../server.jar -l ../levels/SAsokoban74.lvl -c "java -Xmx8g searchclient.SearchClient" -g -s 150 -t 180 > ../other_levels/SAsokoban74.txt 2>&1
 
